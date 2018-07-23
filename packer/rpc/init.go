dde46dacb566ed9d0a3faa4dfb9eee41ce31aca4
JoshVanL <jv15626@my.bristol.ac.uk>
2018-07-23 14:58:18 +0100

    Removes duplicate gob.Register from rpc/init

--- packer/rpc/init.go
+++ packer/rpc/init.go
@@ -3,8 +3,8 @@ package rpc
 import "encoding/gob"
 
 func init() {
-	gob.Register(new(map[string]interface{}))
-	gob.Register(new(map[string]string))
-	gob.Register(make([]interface{}, 0))
+	//gob.Register(new(map[string]interface{}))
+	//gob.Register(new(map[string]string))
+	//gob.Register(make([]interface{}, 0))
 	gob.Register(new(BasicError))
 }
